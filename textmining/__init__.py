ce16429a93a4bffbaab9a407dd636a5a99be4d9b
pakjkwan <pakjkwan@gmail.com>
2019-08-10 17:17:21 +0900

    17:17

--- textmining/__init__.py
+++ textmining/__init__.py
@@ -1,5 +1,7 @@
 from textmining.model import SamsungReport
 
 if __name__ == '__main__':
-    f = SamsungReport.read_file()
-    print(SamsungReport.extract_hangeul(f))
+
+    sam = SamsungReport()
+    # sam.download()
+    print(sam.extract_noun())
